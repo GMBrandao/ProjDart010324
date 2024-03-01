eadea137e683479fdb536fcad01b5a5bf80df498
GMBrandao <gabrielmartbrandao@gmail.com>
2024-03-01 20:42:23 -0300

    Initial commit

---
+++ proj01/bin/proj01.dart
@@ -0,0 +1,5 @@
+import 'package:proj01/proj01.dart' as proj01;
+
+void main(List<String> arguments) {
+  print('Hello world: ${proj01.calculate()}!');
+}
---
+++ proj01/test/proj01_test.dart
@@ -0,0 +1,8 @@
+import 'package:proj01/proj01.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
